18b75287c08e07d883b142114605f32cd587d0d0
Justin Thomas <jthomas54@stu.jsu.edu>
2022-03-02 23:52:49 -0600

    First

---
+++ app/src/main/java/com/example/multi_functional_calculator/CalculatorState.java
@@ -0,0 +1,5 @@
+package com.example.multi_functional_calculator;
+
+public enum CalculatorState {
+    PLUS, MINUS, MULTIPLY, DIVIDE
+}
---
+++ app/src/main/java/com/example/multi_functional_calculator/MainActivity.java
@@ -0,0 +1,350 @@
+package com.example.multi_functional_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.view.View;
+
+
+import com.example.multi_functional_calculator.databinding.ActivityMainBinding;
+
+import java.math.BigDecimal;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private StringBuilder lhs;
+    private boolean isOpSelected = false;
+    private StringBuilder rhs;
+    private BigDecimal rightSide;
+    private BigDecimal leftSide;
+    private String operator;
+    private BigDecimal sign;
+    private Boolean isSign = false;
+    private Boolean isOnLeftSide = false;
+    private Boolean flag = false;
+    private BigDecimal percent;
+    private Boolean percentClicked = false;
+
+
+    private void opSelected (CalculatorState operator) {
+        switch (operator) {
+            case PLUS:
+                if (isSign){
+                    if (isOnLeftSide){
+                        rightSide = new BigDecimal(String.valueOf(rhs));
+                    }
+                }
+                else if (percentClicked){
+                    if (flag){
+                        leftSide = percent;
+                        rightSide = new BigDecimal(String.valueOf(rhs));
+                    }
+                    rightSide = percent;
+                }
+                else {
+                    leftSide = new BigDecimal(String.valueOf(lhs));
+                    rightSide = new BigDecimal(String.valueOf(rhs));
+                }
+                leftSide.add(rightSide);
+                binding.displayId.setText(String.valueOf(leftSide));
+                break;
+
+            case MINUS:
+
+                if (isSign){
+                    if (isOnLeftSide){
+                        rightSide = new BigDecimal(String.valueOf(rhs));
+                    }
+                }
+                else if (percentClicked){
+                    if (flag){
+                        leftSide = percent;
+                        rightSide = new BigDecimal(String.valueOf(rhs));
+                    }
+                    rightSide = percent;
+                }
+                else {
+                    leftSide = new BigDecimal(String.valueOf(lhs));
+                    rightSide = new BigDecimal(String.valueOf(rhs));
+                }
+                leftSide.subtract(rightSide);
+                binding.displayId.setText(String.valueOf(leftSide));
+                break;
+
+            case MULTIPLY:
+                if (isSign){
+                    if (isOnLeftSide){
+                        rightSide = new BigDecimal(String.valueOf(rhs));
+                    }
+                }
+                else if (percentClicked){
+                    if (flag){
+                        leftSide = percent;
+                        rightSide = new BigDecimal(String.valueOf(rhs));
+                    }
+                    rightSide = percent;
+                }
+                else {
+                    leftSide = new BigDecimal(String.valueOf(lhs));
+                    rightSide = new BigDecimal(String.valueOf(rhs));
+                }
+                leftSide.multiply(rightSide);
+                binding.displayId.setText(String.valueOf(leftSide));
+                break;
+
+            case DIVIDE:
+
+                if (isSign){
+                    if (isOnLeftSide){
+                        rightSide = new BigDecimal(String.valueOf(rhs));
+                    }
+                }
+                else if (percentClicked){
+                    if (flag){
+                        leftSide = percent;
+                        rightSide = new BigDecimal(String.valueOf(rhs));
+                    }
+                    rightSide = percent;
+                }
+                else {
+                    leftSide = new BigDecimal(String.valueOf(lhs));
+                    rightSide = new BigDecimal(String.valueOf(rhs));
+                }
+                if (String.valueOf(rhs).equals("0")){
+                    binding.displayId.setText("ERROR");
+                }
+                else{
+                leftSide.divide(rightSide);
+                binding.displayId.setText(String.valueOf(leftSide));
+                }
+                break;
+
+            default:
+                binding.displayId.setText("ERROR");
+                break;
+        }
+
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isOpSelected) {
+                    rhs = new StringBuilder().append("0");
+                } else {
+                    lhs = new StringBuilder().append("0");
+                }
+
+    }
+});
+        binding.btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isOpSelected) {
+                    rhs = new StringBuilder().append("1");
+                } else {
+                    lhs = new StringBuilder().append("1");
+                }
+
+            }
+        });
+        binding.btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isOpSelected) {
+                    rhs = new StringBuilder().append("2");
+                } else {
+                    lhs = new StringBuilder().append("2");
+                }
+
+            }
+        });
+        binding.btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isOpSelected) {
+                    rhs = new StringBuilder().append("3");
+                } else {
+                    lhs = new StringBuilder().append("3");
+                }
+            }
+        });
+        binding.btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isOpSelected) {
+                    rhs = new StringBuilder().append("4");
+                } else {
+                    lhs = new StringBuilder().append("4");
+                }
+
+            }
+        });
+        binding.btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isOpSelected) {
+                    rhs = new StringBuilder().append("5");
+                } else {
+                    lhs = new StringBuilder().append("5");
+                }
+
+            }
+        });
+        binding.btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isOpSelected) {
+                    rhs = new StringBuilder().append("6");
+                } else {
+                    lhs = new StringBuilder().append("6");
+                }
+
+            }
+        });
+        binding.btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isOpSelected) {
+                    rhs = new StringBuilder().append("7");
+                } else {
+                    lhs = new StringBuilder().append("7");
+                }
+            }
+        });
+        binding.btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isOpSelected) {
+                    rhs = new StringBuilder().append("8");
+                } else {
+                    lhs = new StringBuilder().append("8");
+                }
+            }
+        });
+
+        binding.btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isOpSelected) {
+                    rhs = new StringBuilder().append("9");
+                } else {
+                    lhs = new StringBuilder().append("9");
+                }
+            }
+        });
+        binding.btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.displayId.setText("0");
+                leftSide = new BigDecimal("0");
+                rightSide = new BigDecimal("0");
+
+            }
+        });
+        binding.btnDecimal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isOpSelected) {
+                    rhs = new StringBuilder().append(".");
+                } else {
+                    lhs = new StringBuilder().append(".");
+                }
+
+            }
+        });
+        binding.btnDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isOpSelected = true;
+                operator = "DIVIDE";
+
+            }
+        });
+        binding.btnMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isOpSelected = true;
+                operator = "MULTIPLY";
+
+            }
+        });
+        binding.btnEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                opSelected(CalculatorState.valueOf(operator));
+
+            }
+        });
+        binding.btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isOpSelected = true;
+                operator = "MINUS";
+
+            }
+        });
+        binding.btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isOpSelected = true;
+                operator = "PLUS";
+
+            }
+        });
+        binding.btnPercent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (isOpSelected) {
+                    percent = new BigDecimal(String.valueOf(rhs));
+                } else {
+                    percent = new BigDecimal(String.valueOf(lhs));
+                    flag = true;
+                }
+                percent = percent.divide(new BigDecimal(100));
+                percentClicked = true;
+            }
+        });
+        binding.btnSign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isOpSelected) {
+                    sign = new BigDecimal(String.valueOf(rhs));
+                    sign.multiply(new BigDecimal("-1"));
+                    rightSide = sign;
+
+                }
+                else{
+                    sign = new BigDecimal(String.valueOf(lhs));
+                    sign.multiply(new BigDecimal("-1"));
+                    leftSide = sign;
+                    isOnLeftSide = true;
+
+                }
+
+                isSign = true;
+            }
+        });
+        binding.btnSqrt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double amount = 1/2;
+                leftSide = new BigDecimal(String.valueOf(lhs));
+                double sqrt = leftSide.doubleValue();
+                binding.displayId.setText(String.valueOf(Math.pow(sqrt,amount)));
+
+            }
+        });
+    }
+}
